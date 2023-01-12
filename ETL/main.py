c64c802aa61d30d8b5e7296fefb4b8fd89e8d1b6
NoemiAzorin <noemiazorin@hotmail.com>
2023-01-12 22:18:27 +0100

    final update

---
+++ ETL/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import soporte as sp
+
+
+
+
+
+prueba = sp.prueba(5,4)
+print(prueba)
---
+++ ETL/soporte.py
@@ -0,0 +1,11 @@
+import requests
+import pandas as pd
+import numpy as np
+
+
+
+
+def prueba(num1,num2):
+  return num1 * num2
+      
+            
